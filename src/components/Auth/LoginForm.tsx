7c9063f2bc82e2a5602ac35a9d8670686deb8817
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-03-25 09:48:46 -0700

    Validaciones añadidas al formulario

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   View,
   Text,
@@ -6,9 +6,25 @@ import {
   TextInput,
   Button,
   Keyboard
-} from 'react-native'
+} from 'react-native';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function LoginForm() {
+  const formikSubmit = () => {
+    formik.handleSubmit();
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log('Formulario enviado...');
+      console.log(formValue);
+    }
+  });
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesión</Text>
@@ -16,19 +32,39 @@ export default function LoginForm() {
         placeholder='Nombre del usuario'
         style={styles.input}
         autoCapitalize='none'
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue('username', text)}
       />
       <TextInput
         placeholder='Contraseña'
         style={styles.input}
         autoCapitalize='none'
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue('password', text)}
       />
       <Button
         title='Entrar'
-        onPress={() => console.log('Entrando...')}
+        onPress={formikSubmit}
       />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   )
+}
+
+function initialValues() {
+  return {
+    username: '',
+    password: ''
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required('El usuario es obligatorioo'),
+    password: Yup.string().required('La contraseña es obligatoria')
+  };
 }
 
 const styles = StyleSheet.create({
@@ -45,5 +81,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    marginTop: 20
   }
 });
